b0fa246a5a62722edd31f066b38a8a8030c47fe6
vketteni <v.ketteniss@outlook.com>
2023-12-11 20:25:20 +0100

    moved ft_check_* into format/checks/

---
+++ format/checks/checks.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checks.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vincentketteniss <vincentketteniss@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 19:12:15 by vincentkett       #+#    #+#             */
+/*   Updated: 2023/12/11 19:49:02 by vincentkett      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHECKS_H
+#define CHECKS_H
+
+// Include any necessary headers here
+# include "../format.h"
+
+int			ft_check_validity(const char *format, t_format_field *field);
+
+int			ft_check_char_format(const char *format);
+
+int			ft_check_int_format(const char *format);
+
+int			ft_check_str_format(const char *format);
+
+int			ft_check_uint_format(const char *format);
+
+int			ft_check_hexlc_format(const char *format);
+
+int			ft_check_hexuc_format(const char *format);
+
+int			ft_check_ptr_format(const char *format);
+
+#endif 
---
+++ format/checks/ft_check_validity.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_validity.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vincentketteniss <vincentketteniss@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 13:44:00 by vketteni          #+#    #+#             */
+/*   Updated: 2023/12/11 19:47:29 by vincentkett      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checks.h"
+
+int	ft_check_validity(const char *format, t_format_field *field)
+{
+	if (field->specifier == 'c')
+		return (ft_check_char_format(format));
+	else if (field->specifier == 's')
+		return (ft_check_str_format(format));
+	else if (field->specifier == 'p')
+		return (ft_check_ptr_format(format));
+	else if (field->specifier == 'd' || field->specifier == 'i')
+		return (ft_check_int_format(format));
+	else if (field->specifier == 'u')
+		return (ft_check_uint_format(format));
+	else if (field->specifier == 'x')
+		return (ft_check_hexlc_format(format));
+	else if (field->specifier == 'X')
+		return (ft_check_hexuc_format(format));
+	else
+		return (0);
+}

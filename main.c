fc18826cba33d2e0d3fbfe482f6ab8a5f0426712
vketteni <v.ketteniss@outlook.com>
2023-12-13 01:24:28 +0100

    added tests in main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vincentketteniss <vincentketteniss@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 01:34:34 by vincentkett       #+#    #+#             */
-/*   Updated: 2023/12/11 02:32:06 by vincentkett      ###   ########.fr       */
+/*   Updated: 2023/12/13 01:23:47 by vincentkett      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@
 void	test_single_flag(char *format, void *arg)
 {
 	int				d;
-	unsigned int	ud;
+	unsigned int	u;
 	char			*s;
     char            c;
+	void			*p;
+	long long int	x;
+	long long int	X;
 
 	if (format != NULL)
 	{
@@ -26,6 +29,11 @@ void	test_single_flag(char *format, void *arg)
 		{
 			d = *((int *)arg);
 			ft_printf("Received integer: %d\n", d);
+		}
+		else if (format[1] == 'u' )
+		{
+			u = *((unsigned int *)arg);
+			ft_printf("Received unsigned integer: %u\n", u);
 		}
 		else if (format[1] == 'c')
 		{
@@ -36,6 +44,21 @@ void	test_single_flag(char *format, void *arg)
 		{
 			s = (char *)arg;
 			ft_printf("Received string: %s\n", s);
+		}
+		else if (format[1] == 'p')
+		{
+			p = (void *)arg;
+			ft_printf("Received pointer: %p\n", p);
+		}
+		else if (format[1] == 'x')
+		{
+			x = *(long long int *)arg;
+			ft_printf("Received lowercase hex: %x\n", x);
+		}
+		else if (format[1] == 'X')
+		{
+			X = *(long long int *)arg;
+			ft_printf("Received uppercase hex: %X\n", X);
 		}
 		else
 		{
@@ -50,20 +73,32 @@ void	test_single_flag(char *format, void *arg)
 
 int	main(void)
 {
-	char	c;
-	char	*s;
-	int		d;
-	void	*p;
+	int				d;
+	unsigned int	u;
+	char			*s;
+    char            c;
+	void			*p;
+	long long int	x;
+	long long int	X;
 
-	p = &c;
 	c = 'c';
-	s = "world";
 	d = 42;
+	s = "world";
+	u = (unsigned int)d;
+	p = &c;
+	x = (long long int)p;
+	X = x;
 
-	printf("%ld\n", (long)p);
-	printf("%p\n", p);
-	// test_single_flag("%c\n", &c);
-    // test_single_flag("%d\n", &d);
-    // test_single_flag("%s\n", s);
+	// printf("%ld\n", (long)p);
+	// printf("%p\n", p);
+	test_single_flag("%c\n", &c);
+    test_single_flag("%d\n", &d);
+    test_single_flag("%u\n", &u);
+    test_single_flag("%s\n", s);
+    test_single_flag("%p\n", p);
+    test_single_flag("%x\n", &x);
+    test_single_flag("%X\n", &X);
 	return (0);
 }
+
+// cc main.c format/*c checks/*.c print/*.c util/*.c libft/*.c ft_printf.c 
